7cca888656d1b7667d9e07ef0945cba0c5f11137
bugsancho <bugsancho@gmail.com>
2014-07-20 22:35:17 +0300

    Introduced DetonationStrategyFactory

---
+++ Battle-Field-2/BattleFieldGame/Factories/DetonationStrategyFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace BattleFieldGame.Factories
+{
+    using System;
+    using BattleFieldGame.Helpers;
+    using BattleFieldGame.Interfaces;
+    using BattleFieldGame.DetonationStretegies;
+
+    public class DetonationStrategyFactory : IDetonationStrategyFactory
+    {
+        public IMineDetonationStrategy GetDetonationStrategy(MineDetonationType detonationType)
+        {
+            switch (detonationType)
+            {
+                case MineDetonationType.Single:
+                    return new SingleDetonationStrategy();
+                case MineDetonationType.Double:
+                    return new DoubleDetonationStrategy();
+                case MineDetonationType.Triple:
+                    return new TripleDetonationStretegy();
+                case MineDetonationType.Quadriple:
+                    return new QuadripleDetonationStrategy();
+                case MineDetonationType.Quintuple:
+                    return new QuintupleDetonationStrategy();
+                default:
+                    throw new InvalidOperationException("Unsupported detonation type!");
+            }
+        }
+    }
+}
---
+++ Battle-Field-2/BattleFieldGame/Interfaces/IDetonationStrategyFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace BattleFieldGame.Interfaces
+{
+    using BattleFieldGame.Helpers;
+
+    public interface IDetonationStrategyFactory
+    {
+        IMineDetonationStrategy GetDetonationStrategy(MineDetonationType detonationType);
+    }
+}
