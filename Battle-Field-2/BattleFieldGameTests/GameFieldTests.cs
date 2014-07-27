8f622334c3f492f9c73b2aeb312098fb14a8e297
ms-nenova <ms.nenova@gmail.com>
2014-07-27 23:07:57 +0300

    Some initial GameField tests

---
+++ Battle-Field-2/BattleFieldGameTests/GameFieldTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleFieldGame.GameObjects.GameField;
+
+namespace BattleFieldGameTests
+{
+    [TestClass]
+    public class GameFieldTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ZeroSizeTest()
+        {
+            GameField field = new GameField(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void NegativeSizeTest()
+        {
+            GameField field = new GameField(-1);
+        }
+    }
+}
