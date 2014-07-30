1d36e5e09f07b2018d25961f53b07ad9935fbbfe
stiliankanchev <pechi169@gmail.com>
2014-07-31 01:43:41 +0300

    Added: Tests for Single, Double explosion strategies and ConsoleReader

---
+++ Battle-Field-2/BattleFieldGameTests/DetonationStrategies/DoubleDetonationStrategyTests.cs
@@ -0,0 +1,46 @@
+﻿namespace BattleFieldGameTests
+{
+    using BattleFieldGame.DetonationStretegies;
+    using BattleFieldGame.Helpers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DoubleDetonationStrategyTests
+    {
+        [TestMethod]
+        public void DoubleDetonationStrategyExplosionCoordinatesMustMatchToExpected()
+        {
+            var expectedExplosionCoordinates = new List<Coords>()
+            {
+                new Coords(0, -1),
+                new Coords(-1, 0),
+                new Coords(+1, 0),
+                new Coords(0, +1)
+            };
+
+            var doubleDetonationStrategy = new DoubleDetonationStrategy();
+            var returnedExplosionCoordinates = doubleDetonationStrategy.GetExplosionCoordinates();
+
+            CollectionAssert.AreEqual(expectedExplosionCoordinates, returnedExplosionCoordinates);
+        }
+
+        [TestMethod]
+        public void DoubleDetonationStrategySettedMinorStrategyMustAlterTheExplosionCoordinates()
+        {
+            var singleDetonationStrategy = new SingleDetonationStrategy();
+            var doubleDetonationStrategy = new DoubleDetonationStrategy();
+
+            var expectedCoordinates = doubleDetonationStrategy.GetExplosionCoordinates().Select(item => new Coords(item.X, item.Y)).ToList();
+            var originalDoubleDetonationExplosionCoordinates = singleDetonationStrategy.GetExplosionCoordinates().Select(item => new Coords(item.X, item.Y)).ToList();
+            expectedCoordinates.AddRange(originalDoubleDetonationExplosionCoordinates);
+
+            doubleDetonationStrategy.MinorStrategy = singleDetonationStrategy;
+            var outputCoordinates = doubleDetonationStrategy.GetExplosionCoordinates();
+
+            CollectionAssert.AreEqual(expectedCoordinates, outputCoordinates);
+        }
+    }
+}
---
+++ Battle-Field-2/BattleFieldGameTests/DetonationStrategies/SingleDetonationStrategyTests.cs
@@ -0,0 +1,46 @@
+﻿namespace BattleFieldGameTests
+{
+    using BattleFieldGame.DetonationStretegies;
+    using BattleFieldGame.Helpers;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SingleDetonationStrategyTests
+    {
+        [TestMethod]
+        public void SingleDetonationStrategyExplosionCoordinatesMustMatchToExpected()
+        {
+            var expectedExplosionCoordinates = new List<Coords>()
+            {
+                new Coords(-1, -1),
+                new Coords(+1, -1),
+                new Coords(-1, +1),
+                new Coords(+1, +1)
+            };
+
+            var singleDetonationStrategy = new SingleDetonationStrategy();
+            var returnedExplosionCoordinates = singleDetonationStrategy.GetExplosionCoordinates();
+
+            CollectionAssert.AreEqual(expectedExplosionCoordinates, returnedExplosionCoordinates);
+        }
+
+        [TestMethod]
+        public void SingleDetonationStrategySettedMinorStrategyMustAlterTheExplosionCoordinates()
+        {
+            var singleDetonationStrategy = new SingleDetonationStrategy();
+            var doubleDetonationStrategy = new DoubleDetonationStrategy();
+
+            var expectedCoordinates = singleDetonationStrategy.GetExplosionCoordinates().Select(item => new Coords(item.X, item.Y)).ToList();
+            var originalDoubleDetonationExplosionCoordinates = doubleDetonationStrategy.GetExplosionCoordinates().Select(item => new Coords(item.X, item.Y)).ToList();
+            expectedCoordinates.AddRange(originalDoubleDetonationExplosionCoordinates);
+
+            singleDetonationStrategy.MinorStrategy = doubleDetonationStrategy;
+            var outputCoordinates = singleDetonationStrategy.GetExplosionCoordinates();
+
+            CollectionAssert.AreEqual(expectedCoordinates, outputCoordinates);
+        }
+    }
+}
