70803ac840dbd0241015e62d5445e9dc492c8f19
ms-nenova <ms.nenova@gmail.com>
2014-07-30 23:25:37 +0300

    Added more tests for GameField()

--- Battle-Field-2/BattleFieldGameTests/GameFieldTests.cs
+++ Battle-Field-2/BattleFieldGameTests/GameFieldTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BattleFieldGame.GameObjects.GameField;
 
@@ -19,6 +20,36 @@ namespace BattleFieldGameTests
         public void NegativeSizeTest()
         {
             GameField field = new GameField(-1);
+        }
+
+        [TestMethod]
+        public void ValidSizeTest()
+        {
+            int size = 5;
+            GameField field = new GameField(size);
+            Assert.AreEqual(size, field.FieldSize);
+        }
+
+        [TestMethod]
+        public void TestReadFieldSizeAsNumber()
+        {
+            bool isFieldSizeCorrect = false;
+            int fieldSize = 0;
+            string inputData = "6";
+            isFieldSizeCorrect = int.TryParse(inputData, out fieldSize);
+
+            Assert.IsTrue(true, "You haven't entered a number, try again!", isFieldSizeCorrect);
+        }
+
+        [TestMethod]
+        public void TestReadFieldAsString()
+        {
+            bool isFieldSizeCorrect = false;
+            int fieldSize = 0;
+            string inputData = "string input";
+            isFieldSizeCorrect = int.TryParse(inputData, out fieldSize);
+            
+            Assert.IsFalse(false, "You haven't entered a number, try again!", isFieldSizeCorrect);
         }
     }
 }
